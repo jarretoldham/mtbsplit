7b0d131d0d097650058f6f90d182681757f14f5d
Jarret Oldham <jdoldham@breakawayanalytics.co>
2025-06-30 15:55:41 -0400

    adding in tanstack router

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createFileRoute } from '@tanstack/react-router';
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
+import viteLogo from '/vite.svg';
+import './index.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const Route = createFileRoute('/')({
+  component: Index,
+});
+
+function Index() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -30,7 +35,5 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
-
-export default App
